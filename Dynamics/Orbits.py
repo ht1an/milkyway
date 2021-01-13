89441b24d8b1dbb38788cd895244e407fef15011
Hao TIAN <htian_astro@163.com>
2021-01-13 14:43:59 +0800

    update

--- Dynamics/Orbits.py
+++ Dynamics/Orbits.py
@@ -1,6 +1,6 @@
 
 # class SingleStar
-def Calc_Orbit(px, py, pz, vx, vy, vz,t=None, PV_Sun=None, Potential=None):
+def Calc_Orbit(ra, dec, dist, pmra, pmdec, rv,t=None, PV_Sun=None, Potential=None):
     import numpy as np
     from astropy import units
     # from galpy.actionAngle import actionAngleStaeckel
@@ -8,7 +8,7 @@ def Calc_Orbit(px, py, pz, vx, vy, vz,t=None, PV_Sun=None, Potential=None):
     # import galpy.util.bovy_conversion as bc
     from galpy.potential import MWPotential2014
 
-    if PV_Sun == None:
+    if PV_Sun is None:
         U_sun, V_sun, W_sun = 11.1, 12.24, 7.25
         X_sun = 8.3  # Reid et al 2014
         V_LSR = 232
@@ -16,22 +16,17 @@ def Calc_Orbit(px, py, pz, vx, vy, vz,t=None, PV_Sun=None, Potential=None):
         U_sun, V_sun, W_sun = PV_Sun[0:3]
         X_sun = PV_Sun[3]
         V_LSR = PV_Sun[4]
-    if Potential == None:
+    if Potential is None:
         pot = MWPotential2014
     else:
         pot = Potential
 
-    o = Orbit([px/X_sun, py/X_sun, pz/X_sun, vx/V_LSR, vy/V_LSR, vz/V_LSR],ro=X_sun,vo=V_LSR)
-    if t == None:
+    o = Orbit([ra, dec, dist, pmra, pmdec, rv],ro=X_sun, vo=V_LSR,radec=True)
+    if t is None:
+        print("t is none")
         ts = np.linspace(0,10,1001)*units.Gyr
     else:
         ts = t*units.Gyr
     o.integrate(ts, pot=pot)
     return o
 
-px, py, pz = 10, 0, 0
-vx, vy, vz = 230, 0, 0
-Ort = Calc_Orbit(px,py, pz, vx, vy, vz )
-import matplotlib.pyplot as plt
-plt.plot(Ort.x, Ort.y,'k.')
-plt.show()
--- example.py
+++ example.py
@@ -1,4 +1,31 @@
+#%%
 import sys
 sys.path.append('./Dynamics')
 import Orbits as O
+import numpy as np
+from astropy import units
+#%%
+from astropy.io import fits
+fn = "CWY.fits"
+dt = fits.open(fn)
+data = dt[1].data
+ra = data["ra"]
+dec = data["dec"]
+dist = 1/(data["parallax"]+0.027)
+# dist = data["rMedPhotogeo"]/1000
+# dist = data["rMedGeo"]/1000
+rv = 153
+mu_ra = data["pmra"]
+mu_dec = data["pmdec"]
+vx, vy, vz = 0.,230,0. #0, 1, 0
+t = np.linspace(0,5,100001)
+Ort = O.Calc_Orbit(ra[0],dec[0], dist[0], mu_ra[0], mu_dec[0], rv ,t=t)
+#%%
+import matplotlib.pyplot as plt
+Ort.plot(d1="x",d2="y")
+plt.plot(Ort.x(), Ort.y(),'k.')
+plt.show()
 
+Ort.plot(d1="x",d2="z")
+plt.plot(Ort.x(), Ort.z(),'k.')
+plt.show()

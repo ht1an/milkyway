cbe8ef0614b93a24b86af68549fe9d345f7d0227
Hao TIAN <htian_astro@163.com>
2021-01-31 17:40:49 +0800

    update: add the fitting code for density distribution

--- ProfileFit/fit_density.py
+++ ProfileFit/fit_density.py
@@ -5,24 +5,25 @@ the halo:
 """
 import numpy as np
 
+
 def calc_halo_q_mc(R, Z, ns=10001):
     # the parameter and calculation are from the results of
     # Xu et al MNRAS 473, 1244–1257 (2018)
-    sr_max = np.sqrt(R**2+Z**2)   # this is the maximum radius
+    sr_max = np.sqrt(R ** 2 + Z ** 2)  # this is the maximum radius
     # generate the mock equivalent radius for ecllips, sr = sqrt(R**2+(Z/q)**2)
-    sr = np.linspace(0, sr_max, ns) 
+    sr = np.linspace(0, sr_max, ns)
     p1 = 0.887
     p2 = 5.15
     p3 = -7.418
     p4 = 315.79
-    sq = (p1 * sr ** 2 + p2 * sr) / (sr ** 2 + p3 * sr + p4) # the equation in page 1251
-    msr = np.sqrt(R ** 2 + Z ** 2 / (sq ** 2))               # the calculated equivalent radius
+    sq = (p1 * sr ** 2 + p2 * sr) / (sr ** 2 + p3 * sr + p4)  # the equation in page 1251
+    msr = np.sqrt(R ** 2 + Z ** 2 / (sq ** 2))  # the calculated equivalent radius
     diff = np.abs(sr - msr)
     ind_min = np.argmin(diff)
     return sq[ind_min], sr[ind_min], diff[ind_min]
 
 
-def density_halo(R0, Z0, q_halo=None, ns = None):
+def density_halo(R0, Z0, q_halo=None, ns=None):
     n = len(R0)
     if q_halo is None:
         q_halo = np.zeros_like(R0)
@@ -34,21 +35,23 @@ def density_halo(R0, Z0, q_halo=None, ns = None):
                 q_halo[i] = calc_halo_q_mc(R0[i], Z0[i], ns=ns)
     return (R0 / (np.sqrt(R0 ** 2 + (Z0 / q_halo) ** 2))) ** 5
 
+
 def density_disk(Z0, H0, nu0=1):
     return nu0 / (np.cosh((Z0) / (2 * H0))) ** 2
 
+
 def model_DDH(x, R0, y, prange):
     #     obs data
-    Z_obs = y[:, 0]         # the height to the disk
-    rho_obs = y[:, 1]       # density corresponding to Z
+    Z_obs = y[:, 0]  # the height to the disk
+    rho_obs = y[:, 1]  # density corresponding to Z
     # parameters
-    P_tot = x[0]            #    total nu
+    P_tot = x[0]  # total nu
     #     thin disk part
-    f_thin = x[1]          #    fraction of the thin disk
-    H_thin = x[2]          #    scale height of the thin disk
+    f_thin = x[1]  # fraction of the thin disk
+    H_thin = x[2]  # scale height of the thin disk
     #     thick disk part
-    f_thick = x[3]         #    fraction of the thick disk
-    H_thick = x[4]         #    scale height of the thick disk
+    f_thick = x[3]  # fraction of the thick disk
+    H_thick = x[4]  # scale height of the thick disk
     # calculate q for halo
     q_halo = np.zeros_like(Z_obs)
     for i in range(len(Z_obs)):
@@ -63,7 +66,7 @@ def model_DDH(x, R0, y, prange):
     else:
         P_thin_model = 10 ** P_tot * f_thin * density_disk(H_thin, Z_obs)
         P_thick_model = 10 ** P_tot * f_thick * density_disk(H_thick, Z_obs)
-        P_halo_model = 10 ** P_tot * (1-f_thin-f_thick)*density_halo(R0,Z_obs,q_halo)
+        P_halo_model = 10 ** P_tot * (1 - f_thin - f_thick) * density_halo(R0, Z_obs, q_halo)
         #     the chi squre
         lnp = np.sum(-0.5 * (np.log(P_thin_model + P_thick_model + P_halo_model) - np.log(rho_obs)) ** 2)
         return lnp
@@ -71,13 +74,13 @@ def model_DDH(x, R0, y, prange):
 
 def model_DH(x, R0, y, prange):
     #     obs data
-    Z_obs = y[:, 0]         # the height to the disk
-    rho_obs = y[:, 1]       # density corresponding to Z
+    Z_obs = y[:, 0]  # the height to the disk
+    rho_obs = y[:, 1]  # density corresponding to Z
     # parameters
-    P_tot = x[0]            #    total nu
+    P_tot = x[0]  # total nu
     #     disk part
-    f_disk = x[1]          #    fraction of disk
-    H_disk = x[2]          #    scale height of disk
+    f_disk = x[1]  # fraction of disk
+    H_disk = x[2]  # scale height of disk
     # calculate q for halo
     q_halo = np.zeros_like(Z_obs)
     for i in range(len(Z_obs)):
@@ -85,11 +88,11 @@ def model_DH(x, R0, y, prange):
 
     if (P_tot < prange[0, 0]) or (P_tot > prange[0, 1]) or \
             (f_disk < prange[1, 0]) or (f_disk > prange[1, 1]) or \
-            (H_disk < prange[2, 0]) or (H_disk > prange[2, 1]) :
+            (H_disk < prange[2, 0]) or (H_disk > prange[2, 1]):
         return -np.inf
     else:
         P_disk_model = 10 ** P_tot * f_disk * density_disk(H_disk, Z_obs)
-        P_halo_model = 10 ** P_tot * (1-f_disk)*density_halo(R0,Z_obs,q_halo)
+        P_halo_model = 10 ** P_tot * (1 - f_disk) * density_halo(R0, Z_obs, q_halo)
         #     the chi squre
         lnp = np.sum(-0.5 * (np.log(P_disk_model + P_halo_model) - np.log(rho_obs)) ** 2)
         return lnp
@@ -116,7 +119,7 @@ def model_DD(x, R0, y, prange):
     else:
         P_thin_model = 10 ** P_tot * f_thin * density_disk(H_thin, Z_obs)
         P_thick_model = 10 ** P_tot * f_thick * density_disk(H_thick, Z_obs)
-        #     the chi squre
+        #     the chi square
         lnp = np.sum(-0.5 * (np.log(P_thin_model + P_thick_model) - np.log(rho_obs)) ** 2)
         return lnp
 
@@ -124,7 +127,7 @@ def model_DD(x, R0, y, prange):
 def fit_density(R0, y, p0, N, N_burn_in, func, nwalkers, Prange):
     import emcee
     ndim = p0.shape[1]
-    sampler = emcee.EnsembleSampler(nwalkers,ndim, func, args=[R0, y, Prange])
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, func, args=[R0, y, Prange])
     pos, prob, state = sampler.run_mcmc(p0, N_burn_in)
     sampler.reset()
     sampler.run_mcmc(pos, N)
